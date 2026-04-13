d2df69d3d9d404603f3da6f23dbd25e89ac58b0f
CodePicsel <bankarshashank9@gmail.com>
2026-04-13 12:34:23 +0530

    Postman update.

--- KrishiEaze/src/main/java/com/Backend/KrishiEaze/security/CustomAuthenticationProvider.java
+++ KrishiEaze/src/main/java/com/Backend/KrishiEaze/security/CustomAuthenticationProvider.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 @Component
